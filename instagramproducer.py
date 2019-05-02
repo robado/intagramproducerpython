e9ce771b67a5ca35bde2a5d018259496c57ccc1a
Robert Ridal <ridal.robert@gmail.com>
2019-05-03 01:55:31 +0300

    No comment

--- instagramproducer.py
+++ instagramproducer.py
@@ -1,7 +1,7 @@
 from pprint import pprint
 from producer.endpoint import requester
 
-token = '104907068.f67ebf1.3ed44e8f93254c3ba4b50ff03143feaa'  # <----- Insert your Instagram token here
+token = ''  # <----- Insert your Instagram token here
 
 
 class InstagramAPI:
